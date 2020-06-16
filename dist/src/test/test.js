25f839824eb205f28831719c0334fc01faff28da
DDI0720 <rlaskdn3615@gmail.com>
2020-06-16 23:03:29 +0900

    nodemonConfig 해볼차례

---
+++ dist/src/test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+var AA = /** @class */ (function () {
+    function AA() {
+        this.app = 'a';
+        console.log(this.app);
+    }
+    return AA;
+}());
+var bb = new AA();
+//# sourceMappingURL=test.js.map
---
+++ src/test/test.ts
@@ -0,0 +1,8 @@
+class AA {
+    public app = 'a';
+    constructor() {
+        console.log(this.app);
+    }
+}
+
+let bb = new AA();
