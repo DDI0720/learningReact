12faf45ac128af3bb3b9e6e09fba90b9e0359b86
DDI0720 <rlaskdn3615@gmail.com>
2020-06-18 22:47:43 +0900

    리액트로 다시시작

--- www.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import Server from "./src/ts/index";
-
-const server = new Server();
-const app: express.Application = server.getInstance();
-
-app.listen(8080, ()=>{
-    console.log('http://localhost:8080');
-})
