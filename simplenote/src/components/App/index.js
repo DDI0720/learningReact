5f260aa43baa39aade46fa6297fa0a5116d9edc4
DDI0720 <rlaskdn3615@gmail.com>
2020-06-22 23:27:51 +0900

    App 에서 List에 대한 props 설정해주고, 그걸 List 에서 받아 또 대입해줌

--- simplenote/src/components/App/index.js
+++ simplenote/src/components/App/index.js
@@ -6,16 +6,33 @@ import Note from '../Note';
 
 class App extends React.Component {
   state = {
-    notes: [],
-    activeId: null,
+    notes: [
+      {
+        id: '안녕 난 첫번째 노트',
+        title: '그래 난 타이틀1',
+        contents: '내용1'
+      },
+      {
+        id: '히히히히히',
+        title: '제목22',
+        contents: '내용22'
+      },
+      {
+        id: '간장연어장',
+        title: '탄산수',
+        contents: '마요네즈'
+      },
+  ],
+    activeId: '간장연어장',
   }
 
   render() {
+    const { notes, activeId } = this.state;
     return (
       <div className="app">
         <Header />
         <div className="container">
-          <List />
+          <List notes={notes} activeId={activeId}/>
           <Note />
         </div>
       </div>
--- simplenote/src/components/List/index.js
+++ simplenote/src/components/List/index.js
@@ -4,13 +4,20 @@ import ListItem from '../ListItem';
 
 class List extends React.Component {
     render() {
+        const { notes, activeId } = this.props;
         return (
             <div className="list">
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
-                <ListItem />
+                {notes.map((item)=>{
+                    const {id, title, contents} = item;
+                    return (
+                        <ListItem
+                            key = {id}
+                            id = {id}
+                            active = {id === activeId}
+                            title = {title}
+                            contents = {contents}/>
+                    )
+                })}
             </div>
         )
     }
