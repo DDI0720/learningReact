906b53a15a56c2d2b6bac00c903bcb5277b4f255
DDI0720 <rlaskdn3615@gmail.com>
2020-06-23 22:57:22 +0900

    div 클릭시 해당 핸들러 호출될수있도록 추가

--- simplenote/src/components/ListItem/index.js
+++ simplenote/src/components/ListItem/index.js
@@ -3,11 +3,11 @@ import './index.css';
 
 class ListItem extends React.Component {
     render() {
-        const {key, id, active, title, contents} = this.props;
+        const {key, id, active, title, contents, onClick} = this.props;
         return (
-            <div className={active ? "list-item active" : "list-item"}>
+            <div className={active ? "list-item active" : "list-item"} onClick={onClick}>
                 <div className="title">{title ? title : '제목'}</div>
-        <div className="list-item-contents">{contents ? contents : '내용'}</div>
+                <div className="list-item-contents">{contents ? contents : '내용'}</div>
             </div>
         )
     }
