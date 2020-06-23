3edfa20cc1f10896ec29bd211ae8b1fcf31218f0
DDI0720 <rlaskdn3615@gmail.com>
2020-06-23 22:54:05 +0900

    ListItem 클릭시 메서드 실행하도록 추가

--- simplenote/src/components/List/index.js
+++ simplenote/src/components/List/index.js
@@ -4,7 +4,7 @@ import ListItem from '../ListItem';
 
 class List extends React.Component {
     render() {
-        const { notes, activeId } = this.props;
+        const { notes, activeId, onListItemClick} = this.props;
         return (
             <div className="list">
                 {notes.map((item)=>{
@@ -15,7 +15,9 @@ class List extends React.Component {
                             id = {id}
                             active = {id === activeId}
                             title = {title}
-                            contents = {contents}/>
+                            contents = {contents}
+                            onClick={()=>{onListItemClick(id)}}
+                            />
                     )
                 })}
             </div>
