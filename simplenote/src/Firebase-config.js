2e80ba913f70b24ca6648c582c3db82e8305be08
DDI0720 <rlaskdn3615@gmail.com>
2020-06-27 21:35:08 +0900

    ?

--- simplenote/src/Firebase-config.js
+++ simplenote/src/Firebase-config.js
@@ -1,11 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBbjpFGao_yT_C_l0gnUme1rIC8ih5mm-s",
-    authDomain: "simple-note-2a1bd.firebaseapp.com",
-    databaseURL: "https://simple-note-2a1bd.firebaseio.com",
-    projectId: "simple-note-2a1bd",
-    storageBucket: "simple-note-2a1bd.appspot.com",
-    messagingSenderId: "986579464860",
-    appId: "1:986579464860:web:0917d7f9b59cedfaddb359"
 };
 
 export default firebaseConfig;
